61b0ad6e8497d001a85e6c2f7a010faa97049350
mattj241 <londonm241@gmail.com>
2019-04-03 11:10:27 -0400

    Done

--- app/src/main/java/com/android/london/notifs/MainActivity.java
+++ app/src/main/java/com/android/london/notifs/MainActivity.java
@@ -1,5 +1,11 @@
 package com.android.london.notifs;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.CountDownTimer;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
     Spinner spinner;
     Button setCountdown, startCountdown, reset;
     private int[] notifPresets = {1, 5, 10, 20, 30, 60, 90};
+    Integer notificationId = 123456;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
         setCountdown.setOnClickListener(this);
         startCountdown.setOnClickListener(this);
         reset.setOnClickListener(this);
+        createNotificationChannel();
     }
 
     @Override
@@ -90,8 +98,13 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
         }
         else
         {
+            setCountdown.setEnabled(false);
+            startCountdown.setEnabled(false);
             Toast.makeText(v.getContext(), "Countdown Started", Toast.LENGTH_SHORT)
                     .show();
+            int highestNotif = Integer.parseInt(spinner.getSelectedItem().toString());
+            String messageContent = message.getText().toString();
+            initiateCountdown(v, highestNotif, messageContent);
         }
     }
 
@@ -106,6 +119,71 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
         ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, spinnerContents);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(arrayAdapter);
+    }
+
+    //Literally ripped from Android official blog because Android Notifications are REALLY convoluted
+    //https://developer.android.com/training/notify-user/build-notification
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("soleChannel", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void initiateCountdown(final View v, int highestNotif, final String messageContent){
+        final List<Integer> notifs = getNotifParatmeters(highestNotif);
+        new CountDownTimer(highestNotif * 1000, 1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int secondsLeft = (int) (millisUntilFinished);
+                secondsLeft = secondsLeft / 1000;
+                if (notifs.contains(secondsLeft)){
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(v.getContext(), "soleChannel")
+                            .setSmallIcon(R.drawable.ic_notif)
+                            .setContentTitle("Counter Notification")
+                            .setContentText(String.format("%d seconds to countdown", secondsLeft))
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
+                    // notificationId is a unique int for each notification that you must define
+                    notificationManager.notify(notificationId, builder.build());
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(v.getContext(), "soleChannel")
+                        .setSmallIcon(R.drawable.ic_notif)
+                        .setContentTitle("Counter Notification")
+                        .setContentText(String.format("It's time for %s!", messageContent))
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
+                // notificationId is a unique int for each notification that you must define
+                notificationManager.notify(notificationId, builder.build());
+                setCountdown.setEnabled(true);
+                startCountdown.setEnabled(false);
+            }
+        }.start();
+    }
+
+    private List<Integer> getNotifParatmeters(int highestNotif){
+        List<Integer> notifs = new ArrayList<>();
+        for(Integer notif : notifPresets){
+            if (notif <= highestNotif) {
+                notifs.add(notif);
+            }
+        }
+        return notifs;
     }
 
 
